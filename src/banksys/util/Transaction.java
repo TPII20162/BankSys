d26b19d56a2296eda8ee2199774ae1202f0c3aea
Lucas Vasconcelos <lucasvasco8@gmail.com>
2016-11-01 16:20:17 +0000

    Add the constructor, getters, setters

--- src/banksys/util/Transaction.java
+++ src/banksys/util/Transaction.java
@@ -28,4 +28,54 @@ public class Transaction {
 	/** Date when the transaction ocurr . (Java.util)*/
 	private Date date;
 
+	public Transaction(String id, String numberAccount, String type, double value, Date date) {
+		super();
+		this.id = id;
+		this.numberAccount = numberAccount;
+		this.type = type;
+		this.value = value;
+		this.date = date;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNumberAccount() {
+		return numberAccount;
+	}
+
+	public void setNumberAccount(String numberAccount) {
+		this.numberAccount = numberAccount;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
 }
