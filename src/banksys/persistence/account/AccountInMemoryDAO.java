8e78fb555daeecf18b866bc5eda6c1b030cf3ce5
NewtonJ <newtonjoaquim12@gmail.com>
2016-11-10 18:05:38 -0200

    Feitos metodos numberOfAccounts e findByClientId da classe AccountInMemoryDAO, closes #40, closes #39

--- src/banksys/persistence/account/AccountInMemoryDAO.java
+++ src/banksys/persistence/account/AccountInMemoryDAO.java
@@ -1,14 +1,18 @@
 package banksys.persistence.account;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import banksys.model.Account;
+import banksys.model.Client;
 import banksys.persistence.account.exception.AccountCreationException;
 import banksys.persistence.account.exception.AccountDeletionException;
 import banksys.persistence.account.exception.AccountNotFoundException;
 import banksys.persistence.exception.PersistenceException;
 
 public class AccountInMemoryDAO implements AccountDAO {
+
+	private static List<Account> accounts = new ArrayList<Account>();
 
 	@Override
 	public Account create(Account account) throws AccountCreationException {
@@ -41,14 +45,17 @@ public class AccountInMemoryDAO implements AccountDAO {
 
 	@Override
 	public int numberOfAccounts() throws PersistenceException {
-		// TODO Auto-generated method stub
-		return 0;
+		return accounts.size();
 	}
 
 	@Override
 	public List<Account> findByClientId(Double clientId) throws PersistenceException {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Account> accountsById = new ArrayList<Account>();
+		for(Account aux : accounts){
+			if(aux.getClientId() == clientId)
+				accountsById.add(aux);
+		}
+		return accountsById;
 	}
 
 }
