45a69dded2136c8c003b1aa3a94d5d962f6219a1
Lucas Vasconcelos <lucasvasco8@gmail.com>
2016-11-10 20:50:14 +0000

    Criação do metodo delete na ClientInMemoryDAOTest

--- test/banksys/persistence/client/ClientInMemoryDAOTest.java
+++ test/banksys/persistence/client/ClientInMemoryDAOTest.java
@@ -2,6 +2,7 @@ package banksys.persistence.client;
 
 import banksys.model.Client;
 import banksys.persistence.client.exception.ClientCreationException;
+import banksys.persistence.exception.PersistenceException;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -18,5 +19,22 @@ public class ClientInMemoryDAOTest {
 		Client clientReceive = clientDAO.create(client);
 		
 		assertEquals(client, clientReceive);
+	}
+	
+	@Test
+	public void deleteTest() throws PersistenceException{
+		
+		Client client  = new Client((double)1,"Fulano de Tal", "fulano1", "ful1ano");
+		int sizeBefore;
+		
+		ClientInMemoryDAO clientDAO = new ClientInMemoryDAO();
+		
+		clientDAO.create(client);
+		
+		sizeBefore = clientDAO.numberOfClients();
+		
+		clientDAO.delete(client.getId());
+		
+		assertEquals(sizeBefore-1, clientDAO.numberOfClients());
 	}
 }
