2aedc6b12f45008330eb2d7f3e75404cf0a19375
Anderson Gomes <gomes.andrade1@gmail.com>
2016-11-14 17:44:43 -0300

    Chamada de construtor alterada no método AccountInMemoryDAOTest.setUp()

--- test/banksys/persistence/account/AccountInMemoryDAOTest.java
+++ test/banksys/persistence/account/AccountInMemoryDAOTest.java
@@ -23,9 +23,9 @@ public class AccountInMemoryDAOTest {
 	@Before
 	public void setUp() throws Exception {
 		aim = new AccountInMemoryDAO();
-		ac = new Account("1", 0.0, AccountType.ORDINARY, 1.0);
-		ac2 = new Account("2", 0.0, AccountType.SAVINGS, 1.0);
-		ac3 = new Account("3", 0.0, AccountType.ORDINARY, 2.0);
+		ac = new Account(AccountType.ORDINARY, 1.0);
+		ac2 = new Account(AccountType.SAVINGS, 1.0);
+		ac3 = new Account(AccountType.ORDINARY, 2.0);
 	}
 
 	@After
@@ -83,19 +83,18 @@ public class AccountInMemoryDAOTest {
 	}
 
 	@Test
-	public void testList() {
-		try {
-			aim.create(ac);
-			aim.create(ac2);
-			aim.create(ac3);
-		} catch (AccountCreationException e) {}
-		try {
-			assertEquals(aim.list().get(0), ac);
-			assertEquals(aim.list().get(1), ac2);
-			assertEquals(aim.list().get(2), ac3);
-		} catch (PersistenceException e) {}
+	public void testList() throws PersistenceException {
+		
+		aim.create(ac);
+		aim.create(ac2);
+		aim.create(ac3);
+		
+		assertEquals(aim.list().get(0), ac);
+		assertEquals(aim.list().get(1), ac2);
+		assertEquals(aim.list().get(2), ac3);
+		
 	}
-
+	
 	@Test(expected = PersistenceException.class)
 	public void testListIsNull() throws PersistenceException{
 		aim.list();
