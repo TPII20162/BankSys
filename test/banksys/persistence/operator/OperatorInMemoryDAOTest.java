7cce6cc358af9b5f3ea7e7e25dc0b21e96d8d831
Marcos C. Lima <marcos.lima.crn@gmail.com>
2016-11-21 17:06:06 -0300

    test(OperatorInMemoryDAO): implementados testes para os metodos create #93 e retrive #94. Closes #93 #94

--- test/banksys/persistence/operator/OperatorInMemoryDAOTest.java
+++ test/banksys/persistence/operator/OperatorInMemoryDAOTest.java
@@ -1,5 +1,26 @@
 package banksys.persistence.operator;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import banksys.model.Operator;
+import banksys.persistence.operator.exception.OperatorCreationException;
+import banksys.persistence.operator.exception.OperatorNotFoundException;
+
 public class OperatorInMemoryDAOTest {
+	
+	@Test
+	public void testCreate(){
+		OperatorInMemoryDAO opMem = new OperatorInMemoryDAO();
+		Operator op = new Operator("Operator", "operator", "operator");
+		try{
+			opMem.create(op);
+			Operator ret = opMem.retrieve(op.getId());
+			assertTrue("Error: Create Fail", op.getId() == ret.getId());
+		} catch(OperatorCreationException | OperatorNotFoundException e){
+			fail("Error: Create Fail");
+		}
+	}
 
 }
