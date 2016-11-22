5365dc8b410803a98cbeea883f8ce7583d7df44f
Igor Santos Brito <igorsbrito93@gmail.com>
2016-11-22 16:36:33 -0300

    doDebitTest closes #90

--- test/banksys/service/ClientServicesImplTest.java
+++ test/banksys/service/ClientServicesImplTest.java
@@ -39,4 +39,31 @@ public class ClientServicesImplTest {
 		}
 		
 	}
+	
+	
+	@Test
+	public void doDebitTest(){
+		AccountInMemoryDAO accountInMemoryDAO = new AccountInMemoryDAO();
+		try {
+			accountInMemoryDAO.create(new Account("123",AccountType.ORDINARY));
+		} catch (AccountCreationException e) {
+			System.out.println("Erro: "+e.getMessage());
+		}
+		
+		ClientServicesImpl clientServicesImpl = new ClientServicesImpl(accountInMemoryDAO);
+		Client client = new Client(123.,"teste", "teste", "teste");
+		
+		try {
+			clientServicesImpl.doCredit(client, "123", 50.0);
+			clientServicesImpl.doDebit(client, "123", 30.0);
+		} catch (ClientServiceException e) {
+			System.out.println("Erro: "+e.getMessage());
+		}
+		
+		try {
+			assertEquals("Erro valor diferente", 00, 30.,clientServicesImpl.doRetrieveBalance(client, "123"));
+		} catch (ClientServiceException e) {
+			System.out.println("Erro: "+e.getMessage());
+		}
+	}
 }
