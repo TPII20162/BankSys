f7787c4731f5ceed0fc4d86f0b40649486521443
Anderson Gomes <gomes.andrade1@gmail.com>
2016-11-23 12:26:43 -0300

    Classe refatorada: ClientInMemoryDAO

--- src/banksys/persistence/client/ClientInMemoryDAO.java
+++ src/banksys/persistence/client/ClientInMemoryDAO.java
@@ -10,6 +10,7 @@ import banksys.persistence.client.exception.ClientNotFoundException;
 import banksys.persistence.exception.PersistenceException;
 
 public class ClientInMemoryDAO implements ClientDAO {
+	
 	private static double CLIENT_IDS = 1;
 
 	private static List<Client> clients = new ArrayList<Client>();
@@ -17,84 +18,114 @@ public class ClientInMemoryDAO implements ClientDAO {
 	private static double nextId() {
 		return CLIENT_IDS++;
 	}
-
-	public ClientInMemoryDAO() {
-	}
-
+	
 	@Override
 	public Client create(Client client) throws ClientCreationException {
 		
-		for (Client someClient : clients){
-			if (someClient.getId() == client.getId()){
-				throw new ClientCreationException("Error: Duplicate client of ID" + client.getId() + "!");
-			}
-		}
 		client.setId(nextId());
-		ClientInMemoryDAO.clients.add(client);
+		clients.add(client);
+		
 		return client;
+		
 	}
 
 	@Override
 	public void delete(Double id) throws ClientDeletionException {
+		
 		Client client = findById(id);
+		
 		if (client != null) {
-			ClientInMemoryDAO.clients.remove(client);
+			clients.remove(client);
 		} else {
-			throw new ClientDeletionException("Client ID number " + id + " not found!");
+			
+			throw new ClientDeletionException("Client ID number " + id +
+					" not found!");
+			
 		}
+		
 	}
 
 	@Override
 	public Client retrieve(Double id) throws ClientNotFoundException {
+		
 		Client client = findById(id);
+		
 		if (client != null) {
 			return client;
 		} else {
-			throw new ClientNotFoundException("Client ID numer " + id + " not found!");
+			
+			throw new ClientNotFoundException("Client ID numer " + id +
+					" not found!");
+			
 		}
+		
 	}
 
 	@Override
-	public Client retrieveByUsernameAndPassword(String username, String password) throws ClientNotFoundException {
+	public Client retrieveByUsernameAndPassword(String username,
+			String password) throws ClientNotFoundException {
+		
 		Client client = findByUsernameAndPassword(username, password);
+		
 		if (client != null) {
 			return client;
 		} else {
-			throw new ClientNotFoundException("Client username " + username + " not found!");
+			
+			throw new ClientNotFoundException("Client username " + username +
+					" not found!");
+			
 		}
+		
 	}
 
 	@Override
 	public List<Client> list() throws PersistenceException {
-		if (ClientInMemoryDAO.clients.size() == 0) {
+		
+		if (clients.size() == 0) {
 			throw new PersistenceException("No existing client list!");
 		}
+		
 		List<Client> list = new ArrayList<>();
-		list.addAll(ClientInMemoryDAO.clients);
-
+		list.addAll(clients);
+		
 		return list;
+		
 	}
 
 	@Override
 	public int numberOfClients() throws PersistenceException {
-		return ClientInMemoryDAO.clients.size();
+		return clients.size();
 	}
 
 	private Client findById(Double id) {
-		for (Client client : ClientInMemoryDAO.clients) {
-			if (client.getId() == id) {
+		
+		for (Client client : clients) {
+			
+			if (client.getId().equals(id)) {
 				return client;
 			}
+			
 		}
+		
 		return null;
+		
 	}
 
 	private Client findByUsernameAndPassword(String username, String password) {
-		for (Client client : ClientInMemoryDAO.clients) {
-			if (client.getUsername().equals(username) && client.getPassword().equals(password)) {
+		
+		for (Client client : clients) {
+			
+			if (client.getUsername().equals(username) &&
+					client.getPassword().equals(password)) {
+				
 				return client;
+				
 			}
+			
 		}
+		
 		return null;
+		
 	}
+	
 }
