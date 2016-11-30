116144eb8eb573fbfdbd64f8e3402920827ca220
fscfelipe <felipesobreira.c@hotmail.com>
2016-11-29 23:53:10 -0300

    Criado classe de teste para a classe ClientDatabaseDAO

---
+++ test/banksys/persistence/client/ClientDatabaseDAOTest.java
@@ -0,0 +1,43 @@
+package banksys.persistence.client;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ClientDatabaseDAOTest {
+
+	@Before
+	public void setUp(){
+	}
+
+	@After
+	public void tearDown(){
+	}
+	
+	@Test
+	public void testCreate() {
+	}
+
+	@Test
+	public void testDelete(){
+
+	}
+
+	@Test
+	public void testRetrieve() {
+	}
+
+	@Test
+	public void testRetrieveByUsernameAndPassword(){
+	}
+
+	@Test
+	public void testList(){
+	}
+
+	@Test
+	public void testNumberOfClients(){
+	}
+
+}
