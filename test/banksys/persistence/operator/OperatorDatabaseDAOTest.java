d4c22691c3bb5dce11a82fb56cd7a22a30035575
fscfelipe <felipesobreira.c@hotmail.com>
2016-12-11 23:59:19 -0300

    Criado classe de teste para OperatorDatabaseDAO close #143

---
+++ test/banksys/persistence/operator/OperatorDatabaseDAOTest.java
@@ -0,0 +1,39 @@
+package banksys.persistence.operator;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OperatorDatabaseDAOTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreate() {
+	}
+
+	@Test
+	public void testDelete() {
+	}
+
+	@Test
+	public void testRetrieve() {
+	}
+
+	@Test
+	public void testRetrieveByUsernameAndPassword() {
+	}
+
+	@Test
+	public void testList() {
+	}
+
+}
