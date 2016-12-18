f71f8ca5c6823ac79d1a0ee1da1eeda73c48748d
lcarvalhoufc <lcarvalho.developer@gmail.com>
2016-12-18 16:07:35 -0300

    implements testNumberOfClients() close #155

--- test/banksys/persistence/client/ClientDatabaseDAOTest.java
+++ test/banksys/persistence/client/ClientDatabaseDAOTest.java
@@ -1,14 +1,37 @@
-package banksys.persistence.client;
+package banksys.persistence.account;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.security.auth.login.AccountLockedException;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import banksys.model.Account;
+import banksys.model.AccountType;
+import banksys.model.Client;
+import banksys.persistence.ResetSQLiteDataBase;
+import banksys.persistence.account.exception.AccountCreationException;
+import banksys.persistence.account.exception.AccountDeletionException;
+import banksys.persistence.account.exception.AccountNotFoundException;
+import banksys.persistence.client.exception.ClientCreationException;
+import banksys.persistence.client.exception.ClientDeletionException;
+import banksys.persistence.client.exception.ClientNotFoundException;
+import banksys.persistence.client.ClientInMemoryDAO;
+import banksys.persistence.exception.PersistenceException;
 
 
 public class ClientDatabaseDAOTest {
 
+	
 	@Before
-	public void setUp(){
+	public void setUp() throws Exception {
+		ResetSQLiteDataBase.resetDataBase();
 	}
 
 	@After
@@ -38,6 +61,30 @@ public class ClientDatabaseDAOTest {
 
 	@Test
 	public void testNumberOfClients(){
-	}
+		double clientId = 1.0;
+	
+		Client client = new Client(clientId);
+		
+		double clientId = 2.0;
+	
+		Client client2 = new Client(clientId);
+		
+		ClientDatabaseDAO dao = new ClientDatabaseDAO();		
 
+		try {
+			dao.create(client);
+			dao.create(client2);
+			
+			assertEquals(2, dao.numberOfClients());
+			
+		}catch (ClientCreationException e) {
+			System.err.println("Could not create client on database: " + e.getMessage());
+		}catch (ClientNotFoundException e) {
+			System.err.println("Could not find client on database: " + e.getMessage());
+		} catch (PersistenceException e) {
+			System.err.println("Could not list clients: " + e.getMessage());
+		}
+					
+	}
+		
 }
