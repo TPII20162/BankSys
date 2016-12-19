c27acb6e71237d40c943a458304a2597e1bd1376
lcarvalhoufc <lcarvalho.developer@gmail.com>
2016-12-19 00:43:39 -0300

    fix errors in ClientDatabaseDAO

--- src/banksys/persistence/client/ClientDatabaseDAO.java
+++ src/banksys/persistence/client/ClientDatabaseDAO.java
@@ -1,59 +1,61 @@
 package banksys.persistence.client;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import banksys.model.Client;
+import banksys.model.Operator;
+import banksys.model.User;
+import banksys.persistence.Connector;
 import banksys.persistence.client.exception.ClientCreationException;
 import banksys.persistence.client.exception.ClientDeletionException;
 import banksys.persistence.client.exception.ClientNotFoundException;
 import banksys.persistence.exception.PersistenceException;
+import banksys.persistence.user.UserDatabaseDAO;
 
 public class ClientDatabaseDAO implements ClientDAO {
-
-	@Override
+	
+	UserDatabaseDAO userDatabase = new UserDatabaseDAO();
+	
 	public Client create(Client client) throws ClientCreationException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void delete(Double id) throws ClientDeletionException {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public Client retrieve(Double id) throws ClientNotFoundException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public Client retrieveByUsernameAndPassword(String username, String password)
-			throws SQLException {
+	public Client retrieveByUsernameAndPassword(String username, String password) throws ClientNotFoundException {
 		Connection connection = Connector.connect();
 		Client client = null;
-
+		
 		try {
+			User user = userDatabase.retrieveByUsernameAndPassword(username, password);
 			PreparedStatement preparedStatement = connection
-					.prepareStatement("SELECT * FROM client WHERE user_id = ?;");
+					.prepareStatement("SELECT FROM client WHERE user_id = ?;");
 
-			preparedStatement.setString(1, user_id);
+			preparedStatement.setDouble(1, user.getId());
 			
 			ResultSet rs = preparedStatement.executeQuery();
 			
-			if(resultSet.next())
-			{
-				Double clientId = resultSet.getDouble("user_id");
-				client = new Client(clientId);
-				
-			}
+			double user_id = rs.getDouble(1);
+			
+			client = new Client(user_id, user.getFullName(), user.getUsername(), user.getPassword());					
 			preparedStatement.close();
-			connection.close();
-		}
-		catch(SQLException e)
+		}		catch(SQLException e)
 		{
-			throw new SQLException(e.getMessage());
+			throw new ClientNotFoundException(e.getMessage());
 		}
 		return client;
 	}
@@ -64,8 +66,7 @@ public class ClientDatabaseDAO implements ClientDAO {
 		return null;
 	}
 
-	@Override
-	public int numberOfClients() throws SQLException {
+	public int numberOfClients() throws ClientNotFoundException {
 		Integer numberOfClients = 0;
 		Connection connection = Connector.connect();
 
@@ -83,7 +84,7 @@ public class ClientDatabaseDAO implements ClientDAO {
 		}
 		catch(SQLException e)
 		{
-			throw new SQLException(e.getMessage());
+			throw new ClientNotFoundException(e.getMessage());
 		}
 		
 		return numberOfClients;
