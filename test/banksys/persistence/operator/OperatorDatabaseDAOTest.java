f6c7981cbdc146f4ba6ae5e9dca8e1f0a302bf11
MarianaFL <mflgeb@hotmail.com>
2016-12-19 22:29:38 -0300

    métodos quase prontos

--- test/banksys/persistence/operator/OperatorDatabaseDAOTest.java
+++ test/banksys/persistence/operator/OperatorDatabaseDAOTest.java
@@ -2,14 +2,19 @@ package banksys.persistence.operator;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import banksys.model.Operator;
 import banksys.persistence.ResetSQLiteDataBase;
+import banksys.persistence.exception.PersistenceException;
 import banksys.persistence.operator.exception.OperatorCreationException;
 import banksys.persistence.operator.exception.OperatorDeletionException;
+import banksys.persistence.operator.exception.OperatorNotFoundException;
+import junit.framework.Assert;
 
 public class OperatorDatabaseDAOTest {
 
@@ -28,18 +33,20 @@ public class OperatorDatabaseDAOTest {
 	@Test
 	public void testCreate() {
 		
-		Operator op = new Operator(1.0, "Felipe", "fscfelipe", "123");
+		/*Operator op = new Operator(1.0, "Felipe", "fscfelipe", "123");
 		try {
 			operatorDatabase.create(op);
 		} catch (OperatorCreationException e) {
 			fail(e.getMessage());
-		}
+
+		}*/
+					
 	}
 
 	@Test
 	public void testDelete() {
-		
-		/*Operator op = new Operator(7.0, "John", "johnjohn", "77262");
+		/*
+		Operator op = new Operator(7.0, "John", "johnjohn", "77262");
 		
 
 		try {
@@ -49,20 +56,65 @@ public class OperatorDatabaseDAOTest {
 			fail(e.getMessage());
 		} catch (OperatorDeletionException e) {
 			fail(e.getMessage());
-		}*/
-		
+		}
+		*/
 	}
 
 	@Test
 	public void testRetrieve() {
+		/*
+		Operator op = new Operator(7.0, "John", "johnjohn", "77262");
+		double retrievedId;
+		try {
+			operatorDatabase.create(op);
+			retrievedId = operatorDatabase.retrieve(op.getId()).getId();
+			assertEquals("ActualID and retrieved ID should be equals", 7.0, retrievedId, 0);
+		} catch (OperatorCreationException e) {
+			System.err.println("Could not create account on database: " + e.getMessage());
+		} catch (OperatorNotFoundException e) {
+			System.err.println("Could not found operator on database: " + e.getMessage());
+		}
+		*/
 	}
 
 	@Test
 	public void testRetrieveByUsernameAndPassword() {
+		/*
+		Operator op = new Operator(8.0, "Johnny", "jj", "77777");
+		double retrievedId;
+		try {
+			operatorDatabase.create(op);
+			retrievedId = operatorDatabase.retrieveByUsernameAndPassword("jj", "77777").getId();
+			assertEquals("ActualID and retrieved ID should be equals", 8.0, retrievedId, 0);
+		} catch (OperatorCreationException e) {
+			System.err.println("Could not create account on database: " + e.getMessage());
+		} catch (OperatorNotFoundException e) {
+			System.err.println("Could not found operator on database: " + e.getMessage());
+		}
+		*/
 	}
 
 	@Test
 	public void testList() {
-	}
+		Operator op = new Operator(7.0, "John", "johnjohn", "77262");
+		Operator op2 = new Operator(8.0, "Johnny", "jj", "77777");
 
+		try {
+			operatorDatabase.create(op);
+			operatorDatabase.create(op2);
+			List<Operator> operatorList = operatorDatabase.list();
+			assertEquals("Size of the list must be 2",2, operatorList.size());
+			
+			assertEquals(operatorList.get(0).getId(), 77262, 0);
+			assertEquals(operatorList.get(1).getId(), 77777, 0);
+			
+		} catch (OperatorCreationException e) {
+			System.err.println("Could not create account on database: " + e.getMessage());
+		} catch (OperatorNotFoundException e) {
+			System.err.println("Could not found operator on database: " + e.getMessage());
+		} catch (PersistenceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
